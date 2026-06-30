439e0cd3011876e0a0e7173862bc8b0e89e206bf
shashank shekher pandey <shashankpandey10513@gmail.com>
2026-07-01 01:27:20 +0530

    Summer Assignment is Uploaded

--- Day-10/code38.c
+++ Day-10/code38.c
@@ -4,7 +4,7 @@ int main(){
     int i,j,space,rows;
     printf("Enter number of rows:");
     scanf("%d",&rows);
-    for(i=row;i>=1;i--){
+    for(i=rows;i>=1;i--){
         for(space=1;space<=rows-i;space++){
             printf(" ");
         }
